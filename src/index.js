16f390916c7a0293c8ec2d84257c8f265c07b8fc
DimaGenn <44157969+DimaGenn@users.noreply.github.com>
2019-03-20 23:00:42 +0300

    The task Money Exchange  is comleted.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let sum = {};
+    if (currency >= 10000) {
+        sum = { error: "You are rich, my friend! We don't have so much coins for exchange" };
+        return(sum);
+    }
+    else {
+        h = Math.floor(currency / 50);
+        currency = currency % 50;
+
+        q = Math.floor(currency / 25);
+        currency = currency % 25;
+
+        d = Math.floor(currency / 10);
+        currency = currency % 10;
+
+        n = Math.floor(currency / 5);
+        currency = currency % 5;
+
+        p = Math.floor(currency);
+
+        if (h > 0) sum.H = h;
+        if (q > 0) sum.Q = q;
+        if (d > 0) sum.D = d;
+        if (n > 0) sum.N = n;
+        if (p > 0) sum.P = p;
+        return(sum);
+    }
 }
